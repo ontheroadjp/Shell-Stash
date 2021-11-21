942f7f9f5c3d9e70737c0ed41ce409b63f93fe28
ontheroadjp <dev@ontheroad.jp>
2021-11-21 18:27:41 +0900

    [Clean]

--- docs_src/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,9 +0,0 @@
-<template><h1 id="shell-stash" tabindex="-1"><a class="header-anchor" href="#shell-stash" aria-hidden="true">#</a> Shell Stash</h1>
-<p>This script provides a temporary file or directory stash in the shell.
-Files or directories can be moved in and out of this stash from any directory.</p>
-<h2 id="what-s-usuful" tabindex="-1"><a class="header-anchor" href="#what-s-usuful" aria-hidden="true">#</a> What's usuful ?</h2>
-<p>If you install Shell-Stash, you don't need to specify long paths and use mv and cp commands.</p>
-<p>If you use mv or cp commands in the shell, instead of using them, you can simply put them in stash, move the directory, and then immediately remove them from stash.</p>
-<p>However, if you use the cd command every time you want to move a directory, Shell-Stash may not be as effective as you think.</p>
-<p>In that case, you may want to prepare the environment for directory movement first.</p>
-</template>
